1dcf3afb37c2e7987e43608c7fe14cd82b08c94c
julienmalard <julien.malard@mail.mcgill.ca>
2020-04-05 15:13:51 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='லஸ்ஸி-இலக்கணங்கள்',
+    version='0',
+    packages=[''],
+    url='',
+    license='',
+    author='ம. ஜூலீஎன்',
+    author_email='julien.malard@mail.mcgill.ca',
+    description='லஸ்ஸி மென்பொருளுக்காக இலக்கணம் குறிப்புகள்'
+)
