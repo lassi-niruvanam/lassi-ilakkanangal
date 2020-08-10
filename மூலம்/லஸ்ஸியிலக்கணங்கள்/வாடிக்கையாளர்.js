ae731f9a46ea43c1f24b0e65f38d6f847df02788
julienmalard <julien.malard@mail.mcgill.ca>
2020-08-10 17:17:15 -0400

    வேலை செய்கிறது

---
+++ மூலம்/லஸ்ஸியிலக்கணங்கள்/வாடிக்கையாளர்.js
@@ -0,0 +1,23 @@
+grammaire = () => {
+  return
+}
+
+noms_langue = () => {
+  return
+}
+
+ext_langue = () => {
+  return
+}
+
+règles_langue = () => {
+  return
+}
+
+noms_langue = () => {
+  return
+}
+
+noms_langue = () => {
+  return
+}
