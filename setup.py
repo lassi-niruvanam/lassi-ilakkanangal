de1ae5872cff9ba16f5f0ff3bbf01a6e484aa2cf
julienmalard <julien.malard@mail.mcgill.ca>
2020-08-10 11:07:45 -0400

    குறிப்பு

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='லஸ்ஸி-இலக்கணங்கள்',
-    version='0',
-    packages=[''],
+    name='lassi-ilakkanankal',
+    version='0.1.0',
+    packages=find_packages(where="மூலம்"),
+    package_dir={"": "மூலம்"},
     url='',
-    license='',
+    license='AGPL-3.0-or-later',
     author='ம. ஜூலீஎன்',
     author_email='julien.malard@mail.mcgill.ca',
-    description='லஸ்ஸி மென்பொருளுக்காக இலக்கணம் குறிப்புகள்'
+    description='லஸ்ஸி மென்பொருளுக்காக இலக்கணம் குறிப்புகள்',
+    install_requires=['lark-parser']
 )
