9921a4ea59049816f58946e5ec8a7169648b9b9c
julienmalard <julien.malard@mail.mcgill.ca>
2023-01-22 20:15:08 +0100

    முன்னேற்றம்

---
+++ யாகி/மூலம்/நிரல்மொழிகள்.ts
@@ -0,0 +1,31 @@
+// தானாக உருவாக்கப்பட்டது - திருத்தம் செய்ய வேண்டாம்
+import javascript_தகவல்கள் from "@/இலக்கணங்கள்/javascript/தகவல்கள்.json"
+import javascript_மொழிபெயர்ப்புகள் from "@/இலக்கணங்கள்/javascript/மொழிபெயர்ப்புகள்.json"
+import json_தகவல்கள் from "@/இலக்கணங்கள்/json/தகவல்கள்.json"
+import json_மொழிபெயர்ப்புகள் from "@/இலக்கணங்கள்/json/மொழிபெயர்ப்புகள்.json"
+import lark_தகவல்கள் from "@/இலக்கணங்கள்/lark/தகவல்கள்.json"
+import lark_மொழிபெயர்ப்புகள் from "@/இலக்கணங்கள்/lark/மொழிபெயர்ப்புகள்.json"
+import nearley_தகவல்கள் from "@/இலக்கணங்கள்/nearley/தகவல்கள்.json"
+import nearley_மொழிபெயர்ப்புகள் from "@/இலக்கணங்கள்/nearley/மொழிபெயர்ப்புகள்.json"
+import python_தகவல்கள் from "@/இலக்கணங்கள்/python/தகவல்கள்.json"
+import python_மொழிபெயர்ப்புகள் from "@/இலக்கணங்கள்/python/மொழிபெயர்ப்புகள்.json"
+import எழில்_தகவல்கள் from "@/இலக்கணங்கள்/எழில்/தகவல்கள்.json"
+import எழில்_மொழிபெயர்ப்புகள் from "@/இலக்கணங்கள்/எழில்/மொழிபெயர்ப்புகள்.json"
+
+
+export const மொழி_தகவல்கள் = {
+    javascript : javascript_தகவல்கள்,
+    json : json_தகவல்கள்,
+    lark : lark_தகவல்கள்,
+    nearley : nearley_தகவல்கள்,
+    python : python_தகவல்கள்,
+    எழில் : எழில்_தகவல்கள்,
+}
+export const மொழி_மொழிபெயர்ப்புகள் = {
+        javascript : javascript_மொழிபெயர்ப்புகள்,
+    json : json_மொழிபெயர்ப்புகள்,
+    lark : lark_மொழிபெயர்ப்புகள்,
+    nearley : nearley_மொழிபெயர்ப்புகள்,
+    python : python_மொழிபெயர்ப்புகள்,
+    எழில் : எழில்_மொழிபெயர்ப்புகள்,
+    }
